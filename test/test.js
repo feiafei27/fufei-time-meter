36d68a0ca6c7ff47ccf057dcacf1a2e6d4ebdd74
付飞 <2543736927@qq.com>
2020-12-08 09:12:30 +0800

    'interceptor'

--- test/test.js
+++ test/test.js
@@ -1,16 +1,10 @@
 var timeMeter = require('../index');
 
 var instance = new timeMeter({
-  intervalSpace:1000,
+  intervalSpace:10,
   timeUpdate(e){
     console.log(e);
   }
 })
-let myInterceptor = instance.resultInterceptor.add(function (result) {
-  result.second = result.milliseconds / 1000;
-  return result;
-})
-
-instance.resultInterceptor.remove(myInterceptor);
 
 instance.start();
